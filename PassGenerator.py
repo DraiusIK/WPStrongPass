2cc80b682824e3f72e8d7efb38e720fdefa18e34
DraiusIK <122639543+DraiusIK@users.noreply.github.com>
2023-01-27 01:04:43 -0300

    Add files via upload

---
+++ PassGenerator.py
@@ -0,0 +1,25 @@
+import random
+import string
+
+def generate_password(pass_number, pass_length):
+    chars = string.ascii_letters + string.digits + string.punctuation
+    for i in range(pass_number):
+        password = ''.join(random.choice(chars) for _ in range(pass_length))
+        print(f'SENHA {i+1} GERADA: {password}')
+
+def banner():
+    print("****************************************")
+    print("$                                      $")
+    print("$    Bem-vindo ao Gerador de Senhas    $")
+    print("$                                      $")
+    print("****************************************")
+
+if __name__ == '__main__':
+    banner()
+    pass_number = int(input("NÚMERO DE SENHAS GERADAS: "))
+    pass_length = int(input("QUANTIDADE DE CARACTERES: "))
+    if pass_length < 1 or pass_number < 1:
+        print("O NÚMERO E O COMPRIMENTO DEVEM SER POSITIVOS")
+    else:
+        generate_password(pass_number, pass_length)
+
